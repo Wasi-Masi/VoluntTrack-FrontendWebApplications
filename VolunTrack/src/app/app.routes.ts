941de75f6fbdc28fbcb58a501af1cfbeacae8535
kcc12321 <cassiusmartel21042006@gmail.com>
2025-05-13 18:09:34 -0500

    Add calendar, time and locations icons

--- VolunTrack/src/app/app.routes.ts
+++ VolunTrack/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { DashboardComponent } from './dashboard/components/dashboard.component';
 import { VolunteersComponent } from './volunteers/components/volunteers.component';
 import { ProfileComponent } from './profile/components/profile.component';
 import {ActivityDetailsComponent} from './activity-details/components/activity-details.component';
+import {RegisteredVolunteersComponent} from './registered-volunteers/components/registered-volunteers.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'volunteers', component: VolunteersComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'activity/:id', component: ActivityDetailsComponent}
+  { path: 'activity/:id', component: ActivityDetailsComponent},
+  { path: 'registered/:id', component: RegisteredVolunteersComponent}
+
 ];
 
 export const appRoutes = provideRouter(routes);
