b102f581b24e9e82fd4a2b319270d3ab884dc497
kcc12321 <cassiusmartel21042006@gmail.com>
2025-05-12 20:03:06 -0500

    Add routing

--- VolunTrack/src/app/shared/toolbar/toolbar.component.ts
+++ VolunTrack/src/app/shared/toolbar/toolbar.component.ts
@@ -3,6 +3,7 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar-component',
@@ -19,7 +20,21 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+  constructor(private router: Router) {}
+
+  goToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  goToVolunteers() {
+    this.router.navigate(['/volunteers']);
+  }
+
+  goToProfile() {
+    this.router.navigate(['/profile']);
+  }
+
   openNotifications() {
-    // Aquí luego se mostrará el popup de notificaciones
+    // Aquí puedes abrir un modal o popup de notificaciones
   }
 }
