3b9bb5b201bf4d50779c0cd9dcc7195dde234afe
MarceloHkd <alejandro.binda@gmail.com>
2025-05-16 01:24:50 -0500

    feat: Add account, update account, register and login

--- VolunTrack/src/app/app.routes.ts
+++ VolunTrack/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { provideRouter, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/components/dashboard.component';
 import { VolunteersComponent } from './volunteers/components/volunteers.component';
-import { ProfileComponent } from './profile/components/profile.component';
 import {ActivityDetailsComponent} from './activity-details/components/activity-details.component';
 import {RegisteredVolunteersComponent} from './registered-volunteers/components/registered-volunteers.component';
+import { RegisterComponent } from './register/components/register.component';
+import { LoginComponent } from './login/components/login.component';
+import { ProfileComponent } from './profile/components/profile.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  //{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'volunteers', component: VolunteersComponent },
   { path: 'profile', component: ProfileComponent },
