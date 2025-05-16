baf2dcc02625d4aedba15a6db0c309a3c3758885
kcc12321 <cassiusmartel21042006@gmail.com>
2025-05-15 22:03:48 -0500

    Add volunteer card

--- VolunTrack/src/app/volunteers/components/volunteers.component.ts
+++ VolunTrack/src/app/volunteers/components/volunteers.component.ts
@@ -1,19 +1,37 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatFormField, MatInput, MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSelect, MatSelectModule} from '@angular/material/select';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from '@angular/material/datepicker';
+import {MatNativeDateModule, MatOption} from '@angular/material/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent, MatDialogActions
+} from '@angular/material/dialog';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 
-import {VolunteersService} from '../services/volunteers.service';
-import {Volunteer} from '../model/volunteers.entity';
-import {MatButton} from '@angular/material/button';
-import {NgIf} from '@angular/common';
+import { VolunteersService } from '../services/volunteers.service';
+import { Volunteer } from '../model/volunteers.entity';
+import {DatePipe, NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-volunteers',
@@ -29,8 +47,10 @@ import {NgIf} from '@angular/common';
     ReactiveFormsModule,
     FormsModule,
     MatPaginatorModule,
-    MatButton,
-    NgIf
+    MatDialogModule,
+    MatButtonModule,
+    NgIf,
+    DatePipe
   ],
   templateUrl: './volunteers.component.html',
   styleUrls: ['./volunteers.component.css']
@@ -38,8 +58,6 @@ import {NgIf} from '@angular/common';
 export class VolunteersComponent implements OnInit, AfterViewInit {
   volunteers: Volunteer[] = [];
   displayedColumns: string[] = ['fullName', 'age', 'profession'];
-
-  // Usa MatTableDataSource para facilitar paginación y filtrado
   dataSource = new MatTableDataSource<Volunteer>([]);
 
   searchText: string = '';
@@ -47,16 +65,21 @@ export class VolunteersComponent implements OnInit, AfterViewInit {
   maxAge: number | null = null;
   statusFilter: string = '';
   registrationDateFilter: Date | null = null;
-  showFilters: boolean = false;
-
-
+  selectedVolunteer: Volunteer | null = null;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
   }
 
+  constructor(
+    private volunteersService: VolunteersService,
+    private dialog: MatDialog
+  ) {}
 
-  constructor(private volunteersService: VolunteersService) {}
+
+  selectVolunteer(volunteer: Volunteer): void {
+    this.selectedVolunteer = volunteer;
+  }
 
   ngOnInit(): void {
     this.volunteersService.getVolunteers().subscribe(data => {
@@ -65,7 +88,6 @@ export class VolunteersComponent implements OnInit, AfterViewInit {
       this.applyFilters();
     });
   }
-
 
   applyFilters(): void {
     this.dataSource.data = this.volunteers.filter(v => {
@@ -78,7 +100,96 @@ export class VolunteersComponent implements OnInit, AfterViewInit {
     });
     this.dataSource.paginator = this.paginator;
   }
-  toggleFilters(): void {
-    this.showFilters = !this.showFilters;
+
+  openFilterDialog(): void {
+    const dialogRef = this.dialog.open(VolunteerFilterDialogComponent, {
+      width: '500px',
+      height:'400px',
+      data: {
+        minAge: this.minAge,
+        maxAge: this.maxAge,
+        statusFilter: this.statusFilter,
+        registrationDateFilter: this.registrationDateFilter
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.minAge = result.minAge;
+        this.maxAge = result.maxAge;
+        this.statusFilter = result.statusFilter;
+        this.registrationDateFilter = result.registrationDateFilter;
+        this.applyFilters();
+      }
+    });
   }
 }
+
+@Component({
+  selector: 'app-volunteer-filter-dialog',
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatFormField,
+    MatFormFieldModule,
+    MatInput,
+    MatSelect,
+    MatOption,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDialogActions,
+    MatDatepickerInput,
+    FormsModule,
+    MatButton
+  ],
+  template: `
+    <h2 mat-dialog-title>Filtros</h2>
+    <mat-dialog-content>
+      <mat-form-field appearance="outline" style="width: 100%;">
+        <mat-label>Edad mínima</mat-label>
+        <input matInput type="number" [(ngModel)]="data.minAge"/>
+      </mat-form-field>
+
+      <mat-form-field appearance="outline" style="width: 100%;">
+        <mat-label>Edad máxima</mat-label>
+        <input matInput type="number" [(ngModel)]="data.maxAge"/>
+      </mat-form-field>
+
+      <mat-form-field appearance="outline" style="width: 100%;">
+        <mat-label>Estado</mat-label>
+        <mat-select [(ngModel)]="data.statusFilter">
+          <mat-option value="">Todos</mat-option>
+          <mat-option value="activo">Activo</mat-option>
+          <mat-option value="inactivo">Inactivo</mat-option>
+        </mat-select>
+      </mat-form-field>
+
+      <mat-form-field appearance="outline" style="width: 100%;">
+        <mat-label>Registrado desde</mat-label>
+        <input matInput [matDatepicker]="picker" [(ngModel)]="data.registrationDateFilter"/>
+        <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
+        <mat-datepicker #picker></mat-datepicker>
+      </mat-form-field>
+    </mat-dialog-content>
+
+    <mat-dialog-actions align="end">
+      <button mat-button (click)="onClose()">Cancelar</button>
+      <button mat-raised-button color="primary" (click)="onApply()">Aplicar</button>
+    </mat-dialog-actions>
+  `
+})
+export class VolunteerFilterDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<VolunteerFilterDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+
+  onApply(): void {
+    this.dialogRef.close(this.data);
+  }
+}
+
